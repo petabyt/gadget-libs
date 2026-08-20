29db6cff247c6e00ec27e1957125e8523d57e74a
Daniel C <brikbusters@gmail.com>
2026-08-20 14:43:24 -0400

    add modes, update api

--- dummy/main.c
+++ dummy/main.c
@@ -6,28 +6,21 @@
 #include <wifi.h>
 #include "dummyjpg.h"
 
-//#define SLOW_SCREENS
-//#define SLOW_CONNECTION
-
 struct ModulePriv {
 	int x;
+	int is_slow;
+	int is_live;
 };
 
 static int on_find_connection(struct PakModule *mod, int job) {
 	pak_debug_log(mod, "Connection established");
-#ifdef SLOW_CONNECTION
-	for (int i = 0; i < 10; i++) {
-		if (pak_rt_is_job_cancelled(mod, job)) return PAK_ERR_CANCELLED;
-		pak_rt_set_progress_bar(mod, job, i * 10);
-		usleep(100000);
+	if (mod->priv->is_slow) {
+		for (int i = 0; i < 10; i++) {
+			if (pak_rt_is_job_cancelled(mod, job)) return PAK_ERR_CANCELLED;
+			pak_rt_set_progress_bar(mod, job, i * 10);
+			usleep(100000);
+		}
 	}
-#endif
-
-	pak_rt_save_session_signature(mod, &(struct PakSavedConnection){
-		.name = "DummyDevice",
-		.unique_id = "10:20:30:40",
-		.aux_data = NULL,
-	});
 
 	return 0;
 }
@@ -38,35 +31,52 @@ static int init(struct PakModule *mod) {
 	pak_rt_set_session_property(mod, PAK_PROP_NAME, "Dummy Device");
 	pak_rt_set_session_property(mod, PAK_PROP_FW_VER, "v1.2.3");
 
-	pak_rt_set_widget(mod, &(struct PakWidget) {
-			.name = "button",
-			.title = "A Button",
-			.type = PAK_BUTTON,
-	});
+	const char *setup_option = pak_rt_get_setup_option(mod) == NULL ? "none" : pak_rt_get_setup_option(mod);
 
-	pak_rt_set_widget(mod, &(struct PakWidget) {
-			.name = "bool",
-			.title = "A Switch",
-			.type = PAK_BOOLEAN,
-			.u.boolv.v = 1,
-	});
+	if (!strcmp(setup_option, "slow") || !strcmp(setup_option, "camera")) {
+		if (!strcmp(setup_option, "slow")) mod->priv->is_slow = 1;
+		pak_rt_set_screen_supported(mod, PAK_SCREEN_DASHBOARD, 1);
+		pak_rt_set_screen_supported(mod, PAK_SCREEN_FILE_GALLERY, 1);
+		pak_rt_set_screen_supported(mod, PAK_SCREEN_FILE_VIEWER, 1);
+		pak_rt_set_screen_supported(mod, PAK_SCREEN_LIVEVIEW, 1);
+		pak_rt_set_screen_supported(mod, PAK_SCREEN_INTERVALOMETER, 1);
+		pak_rt_set_storage_info(mod, "sdcard", &(struct PakStorageInfo){
+			.sorted_by = PAK_NEWEST_FIRST,
+			.n_files_total = 50,
+		});
 
-	pak_rt_set_widget(mod, &(struct PakWidget) {
-			.name = "dropdown",
-			.title = "Option",
-			.type = PAK_DROPDOWN,
-			.u.dropdownv = {
-					.index_value = 1,
-					.list = (const char *[]) {"4.0l I6", "5.6l v8", "7.4l v8", "2.8l tdi", NULL}
-			}
-	});
+		pak_rt_set_widget(mod, &(struct PakWidget) {
+				.name = "button",
+				.title = "A Button",
+				.type = PAK_BUTTON,
+		});
 
-	pak_rt_set_screen_supported(mod, PAK_SCREEN_DASHBOARD, 1);
-	pak_rt_set_screen_supported(mod, PAK_SCREEN_FILE_GALLERY, 1);
-	pak_rt_set_screen_supported(mod, PAK_SCREEN_FILE_VIEWER, 1);
-	pak_rt_set_screen_supported(mod, 107, 1);
+		pak_rt_set_widget(mod, &(struct PakWidget) {
+				.name = "bool",
+				.title = "A Switch",
+				.type = PAK_BOOLEAN,
+				.u.boolv.v = 1,
+		});
 
-	pak_rt_set_storage_info(mod, "sdcard", 50, PAK_NEWEST_FIRST);
+		pak_rt_set_widget(mod, &(struct PakWidget) {
+				.name = "dropdown",
+				.title = "Option",
+				.type = PAK_DROPDOWN,
+				.u.dropdownv = {
+						.index_value = 1,
+						.list = (const char *[]) {"4.0l I6", "5.6l v8", "7.4l v8", "2.8l tdi", NULL}
+				}
+		});
+	} else if (!strcmp(setup_option, "tethered")) {
+		pak_rt_set_screen_supported(mod, PAK_SCREEN_DASHBOARD, 1);
+		pak_rt_set_screen_supported(mod, PAK_SCREEN_LIVE_FEED, 1);
+		pak_rt_set_storage_info(mod, "live", &(struct PakStorageInfo){
+			.n_files_total = 1,
+			.is_live = 1,
+		});
+		mod->priv->is_live = 1;
+	}
+
 	pak_rt_set_tick_interval(mod, 1000 * 100);
 	return 0;
 }
@@ -78,6 +88,17 @@ static int on_try_connect_wifi(struct PakModule *mod, struct PakWiFiAdapter *han
 static int on_idle_tick(struct PakModule *mod, unsigned int us_since_last_tick) {
 	pak_rt_set_session_property_int(mod, PAK_PROP_BATTERY_MAIN, mod->priv->x);
 	if ((mod->priv->x += 10) > 100) mod->priv->x = 0;
+
+	if (mod->priv->is_live) {
+		struct PakFileHandle file = { .index_in_view = 0, .storage_name = "live" };
+		pak_rt_add_file_metadata(mod, &file, &(struct PakFileMetadata){
+			.filename = "live.jpg",
+			.mime_type = "image/jpeg",
+		});
+		pak_rt_add_file_contents(mod, &file, _dummy_jpeg_jpg, sizeof(_dummy_jpeg_jpg), 0, sizeof(_dummy_jpeg_jpg));
+		mod->priv->is_live = 0;
+	}
+
 	return 0;
 }
 
@@ -86,16 +107,16 @@ static int on_disconnect(struct PakModule *mod) {
 }
 
 static int on_switch_screen(struct PakModule *mod, int old_screen, int new_screen, int job) {
-	pak_global_log("dummymod: Switching screen (%d -> %d)", old_screen, new_screen);
-#ifdef SLOW_SCREENS
-	if (new_screen == PAK_SCREEN_FILE_VIEWER) {
-		for (int i = 0; i < 100; i++) {
-			if (pak_rt_is_job_cancelled(mod, job)) return 0;
-			pak_rt_set_progress_bar(mod, job, i);
-			usleep(10000);
+	pak_debug_log(mod, "dummymod: Switching screen (%d -> %d)", old_screen, new_screen);
+	if (mod->priv->is_slow) {
+		if (new_screen == PAK_SCREEN_FILE_VIEWER) {
+			for (int i = 0; i < 100; i++) {
+				if (pak_rt_is_job_cancelled(mod, job)) return 0;
+				pak_rt_set_progress_bar(mod, job, i);
+				usleep(10000);
+			}
 		}
 	}
-#endif
 	return 0;
 }
 
@@ -137,13 +158,16 @@ static int on_request_file_metadata(struct PakModule *mod, int job, struct PakFi
 	return 0;
 }
 
+static int on_request_liveview_frame(struct PakModule *mod, int job, struct PakFileHandle *handle) {
+	return pak_rt_add_file_contents(mod, handle, _dummy_jpeg_jpg, sizeof(_dummy_jpeg_jpg), 0, 0);
+}
+
 static int on_run_test(struct PakModule *mod, int screen, int job) {
 	return 0;
 }
 
 static int on_custom_command(struct PakModule *mod, int job, int argc, const char * const *argv) {
 	if (!strcmp(argv[0], "help")) {
-		//pak_rt_disconnect(mod, "Done");
 		pak_global_log("Foo fighters");
 	}
 	return 0;
@@ -154,11 +178,11 @@ static int on_prop_changed(struct PakModule *mod, int job, struct PakWidget *pro
 	return 0;
 }
 
-int get_module_dummy(struct PakModule *mod) {
+int get_module(struct PakModule *mod) {
 	mod->init = init;
 	mod->on_request_file_thumbnail = on_request_thumbnail;
 	mod->on_request_file_metadata = on_request_file_metadata;
-	//mod->on_try_connect_wifi = on_try_connect_wifi;
+	mod->on_request_liveview_frame = on_request_liveview_frame;
 	mod->on_request_file_contents = on_request_file_contents;
 	mod->on_find_connection = on_find_connection;
 	mod->on_idle_tick = on_idle_tick;
@@ -168,4 +192,3 @@ int get_module_dummy(struct PakModule *mod) {
 	mod->on_setting_changed = on_prop_changed;
 	return 0;
 }
-__attribute__((weak)) int get_module(struct PakModule *mod) { return get_module_dummy(mod); }
